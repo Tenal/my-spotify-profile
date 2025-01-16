2825d82d65da3c597a4e8f64c5b30065f9de5bea
tenal <tenalbourchier@gmail.com>
2025-01-16 15:47:43 -0500

    add login page message

--- app/components/LoginPage.tsx
+++ app/components/LoginPage.tsx
@@ -3,7 +3,17 @@ import Link from "next/link"
 export function LoginPage() {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-[hsl(var(--background))] text-[hsl(var(--foreground))]">
-            <h2 className="text-3xl font-extrabold mb-8">My Spotify Profile</h2>
+            <h2 className="text-3xl font-extrabold mb-6 text-center">
+                My Spotify Profile
+            </h2>
+            <p className="p-3 max-w-[300px] text-center">
+                Welcome! To access this app, you&apos;ll need a Spotify account.
+                Click login below to authenticate with Spotify.
+            </p>
+            <p className="text-xs mb-8 p-3 max-w-[300px] text-center">
+                This app is a personal learning project, it is not affiliated
+                with or endorsed by Spotify.
+            </p>
             <Link href="/api/auth/login">
                 <button className="px-8 py-3 bg-[hsl(var(--accent))] text-[hsl(var(--background-active))] uppercase font-medium tracking-wide rounded-full hover:bg-[hsl(var(--accent-hover))] hover:scale-105 transition-all duration-200">
                     Log in to Spotify
