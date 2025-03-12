05ff5d72e540dc9ef81e42dce69a173c51d1ba94
tenal <tenalbourchier@gmail.com>
2025-03-12 13:17:44 -0400

    fix package issue & update login screen messaging

--- app/components/LoginPage.tsx
+++ app/components/LoginPage.tsx
@@ -7,12 +7,16 @@ export function LoginPage() {
                 My Spotify Profile
             </h2>
             <p className="p-3 max-w-[300px] text-center">
-                Welcome! To access this app, you&apos;ll need a Spotify account.
-                Click login below to authenticate with Spotify.
+                Welcome! To access this app, you&apos;ll need a Spotify account
+                & for me to grant access to your account. Following that, you
+                can click login below to authenticate with Spotify.
             </p>
             <p className="text-xs mb-8 p-3 max-w-[300px] text-center">
-                This app is a personal learning project, it is not affiliated
-                with or endorsed by Spotify.
+                Important: due to the Spotify API&apos;s terms of service, I
+                need to manually grant access to your account. Without this, the
+                My Spotify Profile app won&apos;t display any of your data.
+                I&apos;m happy to grant access to anyone who wants access, just
+                send your name & email to tenalbourchier@gmail.com!
             </p>
             <Link href="/api/auth/login">
                 <button className="px-8 py-3 bg-[hsl(var(--accent))] text-[hsl(var(--background-active))] uppercase font-medium tracking-wide rounded-full hover:bg-[hsl(var(--accent-hover))] hover:scale-105 transition-all duration-200">
